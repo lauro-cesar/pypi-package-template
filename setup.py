e5c69ee81523f018463b94de9a8aedfc3f5952c5
Lauro C Oliveira <lauro@hostcert.com.br>
2021-07-15 12:29:41 -0300

    House keep

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-"""A setuptools based setup module.
-
+"""
+A setuptools based setup module.
 See:
 https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
@@ -18,29 +18,10 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
     name='sampleproject',  # Required
-
-    # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='2.0.0',  # Required
-
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
     description='A sample Python project',  # Optional
 
